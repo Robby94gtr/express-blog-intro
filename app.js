4a46b59e83d318b56144a0c97c1937eb0806593f
Robby94gtr <robertogramazio94@gmail.com>
2025-04-17 03:07:18 +0200

    [UPD] Added Array

--- app.js
+++ app.js
@@ -10,10 +10,45 @@ const app = express();
 
 const port = 3000;
 
+// impostazione dello strumento per recuperare gli asset statici
+app.use(express.static('public'));
+
 // definizione della rotta base
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Benvenuti');
+})
+
+app.get('/menudeidolci', (req, res) => {
+    const menuDeiDolci = [
+        {
+            title: "Ciambellone",
+            content: "Ciambellone",
+            image: "<img src=./imgs/ciambellone.jpeg>",
+            tags: ["tag1", "tag2", "tag3"]
+        },{
+            title: "Cracker Barbabietola",
+            content: "Cracker",
+            image: "<img src=./imgs/cracker_barbabietola.jpeg>",
+            tags: ["tag1", "tag2", "tag3"]
+        },{
+            title: "Pane fritto dolce",
+            content: "Pane fritto dolce",
+            image: "<img src=./imgs/pane_fritto_dolce.jpeg>",
+            tags: ["tag1", "tag2", "tag3"]
+        },{
+            title: "Pasta Barbabietola",
+            content: "Pasta Barbabietola",
+            image: "<img src=./imgs/pasta_barbabietola.jpeg>",
+            tags: ["tag1", "tag2", "tag3"]
+        },{
+            title: "Torta Paesana",
+            content: "Torta Paesana",
+            image: "<img src=./imgs/torta_paesana.jpeg>",
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        
+    ]
 })
 
 // inserimento del metodo che lascia in ascolto il server
