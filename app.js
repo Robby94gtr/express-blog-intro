527489ae70d95515af90bc9469f61345eb168f30
Robby94gtr <robertogramazio94@gmail.com>
2025-04-17 03:04:21 +0200

    [UPD] Done App.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+//  importo express
+
+const express = require('express');
+
+// inizializzazione di express nella variabile app
+
+const app = express();
+
+// definizionedel numero di porta
+
+const port = 3000;
+
+// definizione della rotta base
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+// inserimento del metodo che lascia in ascolto il server
+
+app.listen(port, () => {
+    console.log(`server in ascolto`)
+})
